4e665ec7f55b91a21c71326c6941e06780437844
samir <samirjagtap4030@gmail.com>
2024-08-05 10:58:06 +0530

    added 1 test addtocart2

--- src/test/java/AddToCart.java
+++ src/test/java/AddToCart.java
@@ -1,4 +1,21 @@
 
 public class AddToCart {
+	
+	public void AddToCart() {
+		
+		System.out.println("AddToCart");
+		
+		// take basic folder structure of project by cloning in any folder 
+		// we will create feature branch that is addtocart
+		// we will write code and push code to addtocart branch
+		// lead will review code and comment it 
+		// we will update code and again send for review
+		// if code is correct then we will do merge pull request to master 
+	}
+	public void AddToCart2() {    
+		
+		System.out.println("AddToCart2");
+		
+	}
 
 }
